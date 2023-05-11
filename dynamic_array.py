ce4906682656c522b0f6202fa800498edc1f92ce
justinmillet <justinmillet1@gmail.com>
2023-05-10 21:34:43 -0700

    DA

---
+++ dynamic_array.py
@@ -0,0 +1,193 @@
+# Name:
+# OSU Email:
+# Course: CS261 - Data Structures
+# Assignment:
+# Due Date:
+# Description:
+
+
+from static_array import StaticArray
+
+
+class DynamicArrayException(Exception):
+    """
+    Custom exception class to be used by Dynamic Array
+    DO NOT CHANGE THIS CLASS IN ANY WAY
+    """
+    pass
+
+
+class DynamicArray:
+    def __init__(self, start_array=None):
+        """
+        Initialize new dynamic array
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        self._size = 0
+        self._capacity = 4
+        self._data = StaticArray(self._capacity)
+
+        # populate dynamic array with initial values (if provided)
+        # before using this feature, implement append() method
+        if start_array is not None:
+            for value in start_array:
+                self.append(value)
+
+    def __str__(self) -> str:
+        """
+        Return content of dynamic array in human-readable form
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        out = "DYN_ARR Size/Cap: "
+        out += str(self._size) + "/" + str(self._capacity) + ' ['
+        out += ', '.join([str(self._data[_]) for _ in range(self._size)])
+        return out + ']'
+
+    def __iter__(self):
+        """
+        Create iterator for loop
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        self._index = 0
+        return self
+
+    def __next__(self):
+        """
+        Obtain next value and advance iterator
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        try:
+            value = self[self._index]
+        except DynamicArrayException:
+            raise StopIteration
+
+        self._index += 1
+        return value
+
+    def get_at_index(self, index: int) -> object:
+        """
+        Return value from given index position
+        Invalid index raises DynamicArrayException
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        if index < 0 or index >= self._size:
+            raise DynamicArrayException
+        return self._data[index]
+
+    def set_at_index(self, index: int, value: object) -> None:
+        """
+        Store value at given index in the array
+        Invalid index raises DynamicArrayException
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        if index < 0 or index >= self._size:
+            raise DynamicArrayException
+        self._data[index] = value
+
+    def __getitem__(self, index) -> object:
+        """
+        Same functionality as get_at_index() method above,
+        but called using array[index] syntax
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        return self.get_at_index(index)
+
+    def __setitem__(self, index, value) -> None:
+        """
+        Same functionality as set_at_index() method above,
+        but called using array[index] syntax
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        self.set_at_index(index, value)
+
+    def is_empty(self) -> bool:
+        """
+        Return True is array is empty / False otherwise
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        return self._size == 0
+
+    def length(self) -> int:
+        """
+        Return number of elements stored in array
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        return self._size
+
+    def get_capacity(self) -> int:
+        """
+        Return the capacity of the array
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        return self._capacity
+
+    def print_da_variables(self) -> None:
+        """
+        Print information contained in the dynamic array.
+        Used for testing purposes.
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        print(f"Length: {self._size}, Capacity: {self._capacity}, {self._data}")
+
+    # -----------------------------------------------------------------------
+
+    def resize(self, new_capacity: int) -> None:
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def append(self, value: object) -> None:
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def insert_at_index(self, index: int, value: object) -> None:
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def remove_at_index(self, index: int) -> None:
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def slice(self, start_index: int, size: int) -> "DynamicArray":
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def merge(self, second_da: "DynamicArray") -> None:
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def map(self, map_func) -> "DynamicArray":
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def filter(self, filter_func) -> "DynamicArray":
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+    def reduce(self, reduce_func, initializer=None) -> object:
+        """
+        TODO: Write this implementation
+        """
+        pass
+
+
+def find_mode(arr: DynamicArray) -> (DynamicArray, int):
+    """
+    TODO: Write this implementation
+    """
+    pass
---
+++ sll.py
@@ -0,0 +1,172 @@
+# Name:
+# OSU Email:
+# Course: CS261 - Data Structures
+# Assignment:
+# Due Date:
+# Description:
+
+
+from SLNode import *
+
+
+class SLLException(Exception):
+    """
+    Custom exception class to be used by Singly Linked List
+    DO NOT CHANGE THIS CLASS IN ANY WAY
+    """
+    pass
+
+
+class LinkedList:
+    def __init__(self, start_list=None) -> None:
+        """
+        Initialize new linked list
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        self._head = SLNode(None)
+
+        # populate SLL with initial values (if provided)
+        # before using this feature, implement insert_back() method
+        if start_list is not None:
+            for value in start_list:
+                self.insert_back(value)
+
+    def __str__(self) -> str:
+        """
+        Return content of singly linked list in human-readable form
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        out = 'SLL ['
+        node = self._head.next
+        while node:
+            out += str(node.value)
+            if node.next:
+                out += ' -> '
+            node = node.next
+        out += ']'
+        return out
+
+    def length(self) -> int:
+        """
+        Return the length of the linked list
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        length = 0
+        node = self._head.next
+        while node:
+            length += 1
+            node = node.next
+        return length
+
+    def is_empty(self) -> bool:
+        """
+        Return True is list is empty, False otherwise
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        return not self._head.next
+
+    # ------------------------------------------------------------------ #
+
+    def insert_front(self, value: object) -> None:
+        """
+        Adds new node at the beginning of the list
+        """
+        new_node = SLNode(value)  # creates new object "SLNode"
+        new_node.next = self._head.next
+        self._head.next = new_node  # updates the new node object
+
+    def insert_back(self, value: object) -> None:
+        """
+        Adds new node at the end of a list
+        """
+        new_node = SLNode(value)  # creates a new object "SLNode"
+        node = self._head
+        while node.next:  # iterates through list until the last node is reached
+            node = node.next
+        node.next = new_node  # inserts the new node at the back
+
+    def insert_at_index(self, index: int, value: object) -> None:
+        """
+        Inserts a value at a specific position in a list
+        """
+        if index < 0 or index > self.length():
+            raise SLLException("Invalid index")  # raises the exception if the index is less than 0 or greater than
+            # the length
+        new_node = SLNode(value)  # creates new object
+        current_node = self._head
+        current_index = 0  # indexs the list at 0
+        while current_index < index:  # iterates through until the object is reached
+            current_node = current_node.next
+            current_index += 1  # adds 1 to the index
+        new_node.next = current_node.next
+        current_node.next = new_node
+
+    def remove_at_index(self, index: int) -> None:
+        """
+        Removes a value at a specified index
+        """
+        if index < 0 or index >= self.length():
+            raise SLLException("Invalid index")
+        current_node = self._head
+        current_index = 0
+        while current_index < index:
+            current_node = current_node.next
+            current_index += 1
+        node_to_remove = current_node.next
+        current_node.next = node_to_remove.next
+        node_to_remove.next = None
+
+    def remove(self, value: object) -> bool:
+        """
+        Removes the first value that matches the given value target
+        """
+        current_node = self._head
+        while current_node.next is not None:
+            if current_node.next.value == value:
+                node_to_remove = current_node.next
+                current_node.next = node_to_remove.next
+                node_to_remove.next = None
+                return True
+            current_node = current_node.next
+        return False
+
+    def count(self, value: object) -> int:
+        """
+        Counts the number of objects that match the determined value
+        """
+        count = 0  # sets the count to 0
+        current_node = self._head.next
+        while current_node:
+            if current_node.value == value:
+                count += 1
+            current_node = current_node.next
+        return count
+
+    def find(self, value: object) -> bool:
+        """
+        Returns a Boolean value based on whether the determined value is in the list
+        """
+        current_node = self._head.next
+        while current_node:
+            if current_node.value == value:
+                return True
+            current_node = current_node.next
+        return False
+
+    def slice(self, start_index: int, size: int) -> "LinkedList":
+        """
+        Returns a new linked list the contains the requested number of nodes from the original list, starting at the
+        requested index
+        """
+        if start_index < 0 or start_index >= self.length():
+            raise SLLException("Invalid start index")
+        if size < 1 or start_index + size > self.length():
+            raise SLLException("Invalid size")
+        new_list = LinkedList()
+        current_node = self._head.next
+        for i in range(start_index):
+            current_node = current_node.next
+        for i in range(size):
+            new_list.insert_back(current_node.value)
+            current_node = current_node.next
+        return new_list
